626cfd3e281bd3c328f209efbecd990a185d5d5a
bigeppo <carlog91cast@gmail.com>
2022-09-28 17:18:27 +0200

    install vue

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::middleware('auth')
 Route::get("{any?}",function(){
     return view("guest.home");
 })->where("any",".*");
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
